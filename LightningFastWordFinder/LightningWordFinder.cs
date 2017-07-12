a61ec4a07ce56eb1c5a6f2dccf87b17956db0dc3
Judit <tolnai.judit12@gmail.com>
2017-07-12 10:53:18 +0200

    a

--- LightningFastWordFinder/LightningWordFinder.cs
+++ LightningFastWordFinder/LightningWordFinder.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace LightningFastWordFinder
 {
     class LightningWordFinder
     {
+        public static string thelongest;
+
         public string GetLongestWord(string text)
         {
-            return "longestWord";
+            int begin = 0;
+            int end = 0;
+            int beginIndex = 0;
+            int endIndex = 0;
+            bool beginOrEnd = true;
+            
+
+            int longest = 0;
+
+            List<string> longestWords = new List<string>();
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (char.IsWhiteSpace(text[i]) || text[i].Equals(@"\s") )
+                {
+                    if (beginOrEnd)
+                    {
+                        begin = i + 1;
+                        beginOrEnd = !beginOrEnd;
+
+                    } else
+                    {
+                        end = i;
+                        beginOrEnd = !beginOrEnd;
+
+                        if (end - begin > longest)
+                        {
+                            longest = end - begin;
+                            beginIndex = begin;
+                            endIndex = end;
+                            string longestWord = text.Substring(beginIndex, (endIndex - beginIndex));
+                            longestWords.Add(longestWord);
+                        }
+
+                    }
+                }
+            }
+
+            
+            for (int i = longestWords.Count - 1; i > 0; i--)
+            {
+                if(Regex.IsMatch(longestWords[i], @"^[a-zA-Z]+$")){
+                    thelongest = longestWords[i];
+                    break;
+                }
+
+            }
+            
+            return thelongest;
         }
     }
 }
